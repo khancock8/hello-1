1f9d69536ccd62ac5e131f9d0f3dfadc2a01afa6
Brent Lucke <blucke1@gmail.com>
2019-01-17 11:11:48 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Brent Lucke
+ * Date: 2018/01/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Brent Lucke\n");
+
+  return 0;
+}
