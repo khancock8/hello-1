d4ef330ff37c1896a589f09f6b5d7bf7446f0bd4
Traggy41 <Traggy41@users.noreply.github.com>
2019-01-17 11:50:31 -0600

    tr

--- hello.c
+++ hello.c
@@ -14,6 +14,7 @@ int main(int argc, char **argv) {
   printf("Marketing\n");
   printf("John Hosch\n");
   printf("Electrical Engineering\n");
-
+  printf("Tristan Ragland");
+  printf("Business Admin");
   return 0;
 }
